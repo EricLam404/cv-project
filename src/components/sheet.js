2ad4aebbd6dab8e934039369ce4308e9afc7c9ca
EricLam404 <lameric9888@gmail.com>
2022-12-23 10:23:17 -0500

    Added personal information to sheet

--- src/components/sheet.js
+++ src/components/sheet.js
@@ -11,9 +11,25 @@ class Sheet extends Component {
     console.table(this.props.form.experience.sections);
     return (
       <div className="sheet">
-        <h1>Description</h1>
-        <h1>Experience</h1>
-        <h1>Education</h1>
+        <div><h1>Personal Information</h1>
+          <div>
+              <div>{"Name: " + this.props.form.personal.name}</div>
+              <div>{"Address: " + this.props.form.personal.address}</div>
+              <div>{"Phone: " + this.props.form.personal.phone}</div>
+              <div>{"Email: " + this.props.form.personal.email}</div>
+              <div>{"Summary: " + this.props.form.personal.summary}</div>
+          </div>
+        </div>
+        <div><h1>Education section</h1>
+          <div>
+
+          </div>
+        </div>
+        <div><h1>Experience section</h1>
+          <div>
+
+          </div>
+        </div>
       </div>
     );
   }
